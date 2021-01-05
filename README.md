4fb7a2e190ef083786fce51f9a0cd24d195f6fee
pawarkrishnali16 <76470734+pawarkrishnali16@users.noreply.github.com>
2021-01-05 15:58:14 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# p5.play-boilerplate
-Boiler plate for p5.play
+# bouncyBall
+Bouncy Ball created using matter.js physics engine
--- sketch.js
+++ sketch.js
@@ -1,38 +1,24 @@
 const Engine = Matter.Engine;
-const World = Matter.World;
+const World= Matter.World;
 const Bodies = Matter.Bodies;
 
-var engine,world;
-var ground;
-var ground_options;
-var ball;
-var ball_options;
-function setup() {
-  createCanvas(400,400);
-  engine = Engine.create();
-  world= engine.world;
+var engine, world;
+var box1, box2;
+var ground1;
 
-  ground_options = {
-    isStatic : true
-  }
-  ball_options = {
-    restitution : 1.0
-  }
-  ball = Bodies.circle(200,200,50,ball_options);
-  World.add(world,ball);
-  ground = Bodies.rectangle(200,380,400,20,ground_options);
-  World.add(world,ground);
-
-  console.log(ground.position.x);
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+    box1=new Box(100,200,50,30);
+    box2=new Box(140,100,50,30);
+    ground1=new Ground(200,390,400,20);
 }
 
-function draw() {
-
-  background("pink"); 
-  Engine.update(engine);
-  rectMode(CENTER) ;
- rect(ground.position.x,ground.position.y,400,20);
- ellipseMode(CENTER) ;
- ellipse(ball.position.x,ball.position.y,50,50);
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    ground1.display();
 }
-
